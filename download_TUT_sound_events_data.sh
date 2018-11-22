1509c186b7b9b9592c3c14949a403ecd5219784c
Zhiting Zhu <zhitingz@cs.utexas.edu>
2018-11-22 17:45:00 -0600

    add script to download training data

---
+++ download_TUT_sound_events_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget https://zenodo.org/record/814831/files/TUT-sound-events-2017-development.audio.1.zip
+wget https://zenodo.org/record/814831/files/TUT-sound-events-2017-development.audio.2.zip
+wget https://zenodo.org/record/814831/files/TUT-sound-events-2017-development.doc.zip
+wget https://zenodo.org/record/814831/files/TUT-sound-events-2017-development.meta.zip
+unzip TUT-sound-events-2017-development.audio.1.zip
+unzip TUT-sound-events-2017-development.audio.2.zip
+unzip TUT-sound-events-2017-development.doc.zip
+unzip TUT-sound-events-2017-development.meta.zip
+
